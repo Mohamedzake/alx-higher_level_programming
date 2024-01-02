99682f3564f5715f8ef9ed52bacf68f9ba5787f6
Mohamedzake <zakem7671@gmail.com>
2024-01-02 08:34:11 -0800

    h

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,2 @@
+#!/usr/bin/python3number = 98
+print(f"{number} Battery street")
---
+++ 0x00-python-hello_world/4-print_float.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+str = "Holberton School"
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINEprint(str * 3)
+print(str[:9])
