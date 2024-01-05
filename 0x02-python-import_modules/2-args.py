a5115eeaccb9d5f0ef16bc1c7403213b1d7178d8
Mohamedzake <zakem7671@gmail.com>
2024-01-04 16:18:01 -0800

    l

--- 0x02-python-import_modules/2-args.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-
-n = len(sys.argv) - 1
-
-print("{} argument{}{}.".format(n, '' if n == 1 else 's', ':' if n > 0 else '.'))
-
-for i in range(1, n + 1):
-    print("{}: {}".format(i, sys.argv[i]))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-import hidden_4
-
-names = [name for name in dir(hidden_4) if not name.startswith("__")]
-for name in sorted(names):
-    print(name)
